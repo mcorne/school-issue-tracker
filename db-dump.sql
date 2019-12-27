ecbd0775dc8999b5dc679cedc9ff4075e9730d0a
Michel Corne <mcorne@yahoo.com>
2019-12-27 15:05:53 +0100

    added sqlite autoincrement

--- db-dump.sql
+++ db-dump.sql
@@ -1,20 +1,18 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE user (
-	id INTEGER NOT NULL, 
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
 	username TEXT NOT NULL, 
 	password TEXT NOT NULL, 
-	PRIMARY KEY (id), 
 	UNIQUE (username), 
 	UNIQUE (password)
 );
 CREATE TABLE post (
-	id INTEGER NOT NULL, 
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
 	author_id INTEGER, 
 	created DATETIME NOT NULL, 
 	title TEXT NOT NULL, 
 	body TEXT NOT NULL, 
-	PRIMARY KEY (id), 
 	FOREIGN KEY(author_id) REFERENCES user (id)
 );
 DELETE FROM sqlite_sequence;
--- sit_app/orm.py
+++ sit_app/orm.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from sit_app import db2
 
+db2.Model.__table_args__ = {"sqlite_autoincrement": True}
+
 
 class User(db2.Model):
-    id = db2.Column(db2.Integer, primary_key=True)
+    id = db2.Column(db2.Integer, primary_key=True, autoincrement=True)
     username = db2.Column(db2.Text, unique=True, nullable=False)
     password = db2.Column(db2.Text, unique=True, nullable=False)
 
@@ -11,7 +13,7 @@ class User(db2.Model):
 
 
 class Post(db2.Model):
-    id = db2.Column(db2.Integer, primary_key=True)
+    id = db2.Column(db2.Integer, primary_key=True, autoincrement=True)
     author_id = db2.Column(db2.Integer, db2.ForeignKey("user.id"))
     created = db2.Column(db2.DateTime, nullable=False, default=datetime.utcnow)
     title = db2.Column(db2.Text, nullable=False)
