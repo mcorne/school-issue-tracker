04ba808fb5529d0b9c7c9ce58e7cc120b9f45d5d
Michel Corne <mcorne@yahoo.com>
2020-01-06 17:56:03 +0100

    replaced register with WTF

--- sit_app/auth.py
+++ sit_app/auth.py
@@ -16,7 +16,7 @@ from flask_login import login_required, login_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from sit_app import db
-from sit_app.forms import LoginForm
+from sit_app.forms import LoginForm, RegisterForm
 from sit_app.orm import User
 from sit_app.helpers import is_safe_url
 
@@ -57,25 +57,26 @@ def logout():
 
 @bp.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+    form = RegisterForm()
+    if form.validate_on_submit():
         error = None
 
-        if not username:
+        if not form.username.data:
             error = _("Username is required")
-        elif not password:
+        elif not form.password.data:
             error = _("Password is required")
-        elif User.query.filter_by(username=username).first() is not None:
-            error = _("User %(username)s already registered", username=username)
+        elif User.query.filter_by(username=form.username.data).first() is not None:
+            error = _(
+                "User %(username)s already registered", username=form.username.data
+            )
 
         if error is None:
-            password = generate_password_hash(password)
-            user = User(username=username, password=password)
+            password = generate_password_hash(form.password.data)
+            user = User(username=form.username.data, password=password)
             db.session.add(user)
             db.session.commit()
             return redirect(url_for("auth.login"))
 
         flash(error)
 
-    return render_template("auth/register.html")
+    return render_template("auth/register.html", form=form)
--- sit_app/forms.py
+++ sit_app/forms.py
@@ -1,10 +1,16 @@
-from flask_babel import _
+from flask_babel import _, lazy_gettext
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField(_("Username"), validators=[DataRequired()])
-    password = PasswordField(_("Password"), validators=[DataRequired()])
-    submit = SubmitField(_("Log In"))
+    username = StringField(lazy_gettext("Username"), validators=[DataRequired()])
+    password = PasswordField(lazy_gettext("Password"), validators=[DataRequired()])
+    submit = SubmitField(lazy_gettext("Log In"))
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(lazy_gettext("Username"), validators=[DataRequired()])
+    password = PasswordField(lazy_gettext("Password"), validators=[DataRequired()])
+    submit = SubmitField(lazy_gettext("Register"))
