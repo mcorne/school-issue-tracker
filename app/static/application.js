4d4a52e17723eed5b784cc104f0012bbef8b1c9c
Michel Corne <mcorne@yahoo.com>
2020-03-12 12:28:25 +0100

    fixed sidebar

---
+++ app/static/application.js
@@ -0,0 +1,9 @@
+function close_sidebar() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("sidebar").style.display = "none";
+}
+
+function open_sidebar() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("sidebar").style.display = "block";
+}
